de618b5d23380eaaf47d352ecd566022a180e3ca
David Tsai <David@whataniceguy.com>
2016-02-05 00:49:08 -0800

    Added switching of film types

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,47 +1,82 @@
 (() => {
   'use strict';
 
+  /**
+   * For the time being, here's how we will "import" our utility/helper functions.
+   */
   const $reduceImgResponse = window.$reduceImgResponse;
   const $on = window.$on;
   const qs = window.qs;
   const $each = window.$each;
-  const testData = window.testData;
 
-  function stubbedLoadImage(data) {
-    window.data = $reduceImgResponse(data);
-    console.log(window.data);
-  }
+  // Variable ref. to our query-selected "main" element, which houses our image grid.
+  const mainContainer = qs('main');
 
-  // function loadImage() {
-  //   fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=437db95c9e8c0340302759555b1f684f&tags=portra%2C+film&sort=relevance&per_page=20&page=1&format=json&nojsoncallback=1')
-  //   .then((response) => {
-  //     return response.json();
-  //   }).then((data) => {
-  //     console.log($reduceImgResponse(data.photos.photo));
-  //   });
-  // }
-
-  const target = qs('main');
-
-  // String.prototype.toHTML = (string) => {
-  //   const container = document.createElement('div');
-  //   container.innerHTML = string;
-  //   return container;
-  // }
-
+  /**
+   * This function allows us to iterate through our data object - which contains
+   * all the necessary information regarding our image. The '$each' function that
+   * I implemented allows us to iterate over the object with ease.
+   */
   function renderImages() {
     $each(window.data, (image) => {
-      let container = document.createElement('div');
-      container.className = 'main-grid-image';
-      let createdImage = document.createElement('img');
-      createdImage.src = image.src;
-      container.appendChild(createdImage);
-      target.appendChild(container);
+      const container = document.createElement('div');
+      container.className = 'grid-image-container';
+
+      const gridImage = document.createElement('div');
+      gridImage.className = 'grid-image';
+      gridImage.style.backgroundImage = `url(${image.src})`;
+
+      container.appendChild(gridImage);
+      mainContainer.appendChild(container);
     });
   }
 
-  // $on(window, 'load', loadImage);
-  $on(window, 'load', stubbedLoadImage(testData.photos.photo));
-  $on(window, 'load', renderImages());
+  /**
+   * Here, we get images from Flickr! Thanks to ES6's friendly string interpolation,
+   * we're able to easily swap out variables - in this case, replacing the search tag
+   * for film type by drawing that information from our selector. The response is
+   * converted to JSON then converted to a more digestible format with a helper function.
+   */
+  function loadImage(filmType) {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=437db95c9e8c0340302759555b1f684f&tags=${filmType}%2C+film&sort=relevance&per_page=10&page=1&format=json&nojsoncallback=1`)
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      window.data = $reduceImgResponse(data.photos.photo);
+    }).then(() => {
+      renderImages();
+    });
+  }
 
+  /**
+   * Function will remove all of grid element's children.
+   * Called whenever we make a change on the type of film we're viewing.
+   */
+  function clearImages() {
+    while (mainContainer.firstChild) {
+      mainContainer.removeChild(mainContainer.firstChild);
+    }
+    mainContainer.textContent = '';
+  }
+
+  /**
+   * When the window is loaded, we'll default the loaded images
+   * to the "Kodak Ektar" set.
+   */
+  $on(window, 'load', () => {
+    loadImage('ektar');
+  });
+
+  /**
+   * Here, we add an event - when a user selects a new film type,
+   * we'll go ahead and clear the main element's children and load
+   * the new ones, requesting the "value" of the film type from Flickr.
+   *
+   * @param  {[type]} qs('.film-selector' [description]
+   * @return {[type]}                     [description]
+   */
+  $on(qs('.film-selector'), 'change', () => {
+    clearImages();
+    loadImage(qs('.film-selector').value);
+  });
 })();
--- app/scripts/helpers.js
+++ app/scripts/helpers.js
@@ -1,30 +1,6 @@
 ((window) => {
   'use strict';
 
-  window.testData = { 'photos': { 'page': 1, 'pages': '441966', 'perpage': 20, 'total': '8839319',
-    'photo': [
-      { "id": '24182835613', 'owner': "47559933@N04", 'secret': "4a120de7eb", 'server': "1517", 'farm': 2, 'title': "", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { "id": '24686255032', 'owner': "47908475@N02", 'secret': "3067db5e5e", 'server': "1464", 'farm': 2, 'title': "", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { "id": '24176710963', 'owner': "57802015@N03", 'secret': "533e5bf505", 'server': "1606", 'farm': 2, 'title': ".", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { "id": '24683517922', 'owner': "30437523@N06", 'secret': "c4ffbbdc31", 'server': "1531", 'farm': 2, 'title': "", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': '24433184999', 'owner': "74315390@N00", 'secret': "e2f9657a89", 'server': "1593", 'farm': 2, 'title': "Citroën HY", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': '24682929852', 'owner': "74315390@N00", 'secret': "c2c71f080d", 'server': "1669", 'farm': 2, 'title': "Peugeot 203", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': '24504840080', 'owner': "7559791@N03", 'secret': "4012f2824b", 'server': "1464", 'farm': 2, 'title': "untitled", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24764962806", 'owner': "116199148@N08", 'secret': "7065a94753", 'server': "1612", 'farm': 2, 'title': "(FILM) Three Hour Tour", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24781913335", 'owner': "92445262@N04", 'secret': "1d5914d674", 'server': "1669", 'farm': 2, 'title': "Changes", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24662440542", 'owner': "74315390@N00", 'secret': "d191aa49fe", 'server': "1587", 'farm': 2, 'title': "Terrot", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': '24412513899', 'owner': "74315390@N00", 'secret': "e7e6f73238", 'server': "1712", 'farm': 2, 'title': "Beetle (II)", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24150617833", 'owner': "68762388@N05", 'secret': "af5a56dd51", 'server': "1602", 'farm': 2, 'title': "[analog] Blick über die Mosel", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24475178660", 'owner': "93323588@N05", 'secret': "a4b37b5d64", 'server': "1690", 'farm': 2, 'title': "Mornings.", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24675492291", 'owner': "47559933@N04", 'secret': "a2e88a58a9", 'server': "1698", 'farm': 2, 'title': "", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24136540483", 'owner': "49191827@N00", 'secret': "984773566b", 'server': "1658", 'farm': 2, 'title': "metropolitan. 2015.", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24644290632", 'owner': "76015142@N04", 'secret': "063ab2f587", 'server': "1624", 'farm': 2, 'title': "La poética en un mar de nubes", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24665820051", 'owner': "74315390@N00", 'secret': "06a5ffec00", 'server': "1655", 'farm': 2, 'title': "Peugeot 404 Cab", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24641383372", 'owner': "74315390@N00", 'secret': "f0e173b1e6", 'server': "1451", 'farm': 2, 'title': "Jaguar XK 150 (III)", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24463436770", 'owner': "74315390@N00", 'secret': "cc4b88df51", 'server': "1694", 'farm': 2, 'title': "Peugeot 302", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 },
-      { 'id': "24388576899", 'owner': "57802015@N03", 'secret': "1273a82d30", 'server': "1483", 'farm': 2, 'title': ".", 'ispublic': 1, 'isfriend': 0, 'isfamily': 0 }
-    ] }, 'stat': 'ok' };
-
   window.qs = (selector, scope) => {
     return (scope || document).querySelector(selector);
   };
@@ -43,7 +19,7 @@
     return window.$parent(element.parentNode, tagName);
   };
 
-  // takes in data array, returns an object
+  // Reduces our data down to an object, with their image id's mapped to the image object.
   window.$reduceImgResponse = (data) => {
     return data.reduce((dataObject, image) => {
       dataObject[image.id] = image;
@@ -52,20 +28,16 @@
     }, {});
   };
 
-
-//      { "id": "24182835613", "owner": "47559933@N04", "secret": "4a120de7eb", "server": "1517", "farm": 2, "title": "", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
-// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-
-  // takes in an image object - flickr response
+  // Takes in an 'image' object and produces the correct source.
   window.$createImgSource = (image) => {
     return (
       `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
     );
   };
 
-  /**
-   * Lodash-like utility functions, written from scratch
-   */
+/**
+ * Lodash-like utility functions, written from scratch.
+ */
   window.$each = (collection, cb) => {
     if (Array.isArray(collection)) {
       for (let i = 0; i < collection.length; i++) {
@@ -87,6 +59,4 @@
 
     return result;
   };
-
-
 })(window);
