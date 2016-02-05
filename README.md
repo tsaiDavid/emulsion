d192797e3d0b6b6cf16221bd1a93dd94392527bd
David Tsai <David@whataniceguy.com>
2016-02-04 18:14:25 -0800

    Fetching data from flickr, created utility/helpers

--- README.md
+++ README.md
@@ -23,3 +23,9 @@ gulp
   - SCSS / CSS using BEM (Block, Element, and Modifiers)
   - ES6 using AirBnB's standard configuration with ESLint
     - The `.eslintrc` is the config file for the linting.
+
+Flickr
+
+https://www.flickr.com/services/api/misc.urls.html
+
+https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,17 @@
 (() => {
   'use strict';
 
-  function yolo() {
-    console.log('hey');
+  function loadImage() {
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=437db95c9e8c0340302759555b1f684f&tags=portra%2C+film&sort=relevance&per_page=20&page=1&format=json&nojsoncallback=1')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log($reduceImgResponse(data.photos.photo));
+    });
   }
 
-  $on(window, 'load', yolo);
+  $on(window, 'load', loadImage);
+
 })();
 
 
