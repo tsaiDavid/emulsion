493b923441c93595a1b88fb923acb7b0082908d9
David Tsai <David@whataniceguy.com>
2016-02-04 19:01:00 -0800

    Rendering photo grid - click disabled

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,42 +1,47 @@
 (() => {
   'use strict';
 
-  function loadImage() {
-    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=437db95c9e8c0340302759555b1f684f&tags=portra%2C+film&sort=relevance&per_page=20&page=1&format=json&nojsoncallback=1')
-    .then((response) => {
-      return response.json();
-    }).then((data) => {
-      console.log($reduceImgResponse(data.photos.photo));
+  const $reduceImgResponse = window.$reduceImgResponse;
+  const $on = window.$on;
+  const qs = window.qs;
+  const $each = window.$each;
+  const testData = window.testData;
+
+  function stubbedLoadImage(data) {
+    window.data = $reduceImgResponse(data);
+    console.log(window.data);
+  }
+
+  // function loadImage() {
+  //   fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=437db95c9e8c0340302759555b1f684f&tags=portra%2C+film&sort=relevance&per_page=20&page=1&format=json&nojsoncallback=1')
+  //   .then((response) => {
+  //     return response.json();
+  //   }).then((data) => {
+  //     console.log($reduceImgResponse(data.photos.photo));
+  //   });
+  // }
+
+  const target = qs('main');
+
+  // String.prototype.toHTML = (string) => {
+  //   const container = document.createElement('div');
+  //   container.innerHTML = string;
+  //   return container;
+  // }
+
+  function renderImages() {
+    $each(window.data, (image) => {
+      let container = document.createElement('div');
+      container.className = 'main-grid-image';
+      let createdImage = document.createElement('img');
+      createdImage.src = image.src;
+      container.appendChild(createdImage);
+      target.appendChild(container);
     });
   }
 
-  $on(window, 'load', loadImage);
+  // $on(window, 'load', loadImage);
+  $on(window, 'load', stubbedLoadImage(testData.photos.photo));
+  $on(window, 'load', renderImages());
 
 })();
-
-
-
-//
-// 'use strict';
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//
-//   const main = document.querySelector('.container');
-//
-//   const obj = Slack.element('div', `
-//     <hr />
-//     <h3> This is a sample header</h3>
-//     <br>
-//     <button>
-//       Click Me
-//     </button>
-//     <hr />
-//   `);
-//
-//   for (var i = 0; i < 10; i++) {
-//     main.appendChild(obj);
-//   }
-//
-//
-//
-// });
